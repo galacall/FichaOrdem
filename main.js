5577f58a7d34227d4dd65b83294f842264c27022
diegosantiago <diegofs2308@gmail.com>
2023-07-27 22:24:22 -0300

    adicionei o objeto ficha

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 class Personagem{
-    constructor(agilidade,forca,intelecto,presenca,vigor){
+    constructor(nome, agilidade,forca,intelecto,presenca,vigor){
+        this.nome = nome;
         this.agilidade = agilidade;
         this.forca = forca;
         this.intelecto = intelecto;
         this.presenca = presenca;
         this.vigor = vigor;
     }
-    
+}
+class Ficha{
+    mostrarConfigFicha(perso){
+	console.log(perso.nome);
+	console.log(perso.agilidade);
+    console.log(perso.forca);
+    console.log(perso.intelecto);
+    console.log(perso.presenca);
+    console.log(perso.vigor);
+    }
 }
