a0f6d231e039ba7c944947008ede2b1273e59da0
diegosantiago <diegofs2308@gmail.com>
2023-07-27 21:52:16 -0300

    adicionei o index.html e o main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+class Personagem{
+    constructor(agilidade,forca,intelecto,presenca,vigor){
+        this.agilidade = agilidade;
+        this.forca = forca;
+        this.intelecto = intelecto;
+        this.presenca = presenca;
+        this.vigor = vigor;
+    }
+    
+}
